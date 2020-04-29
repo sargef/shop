9b71f70736bdd9e7417aa4064fcfb9aac5a4980e
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2020-04-30 05:42:33 +0930

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+// const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+
+const app = express();
+
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    // User.findByPk(1)
+    // .then(user => {
+    //     req.user = user;
+    //     next();
+    // })
+    // .catch(err => console.log(err));
+    next();
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+    app.listen(3000);
+});
---
+++ util/database.js
@@ -0,0 +1,28 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://sargef:Danmicjos12383@cluster0-kpjgv.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(client => {
+        console.log('Successfully connected');
+        _db = client.db();
+        callback();
+    })
+    .catch(err => {
+        console.log(err);
+        throw err;
+    });
+};
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
